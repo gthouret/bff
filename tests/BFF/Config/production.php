41bc30a0f66b5c844ddcaa7dda4f0f792f0a8916
Guy Thouret <guy@monetizer.com>
2018-11-30 18:42:36 +0000

    tests/BFF/Config/production.php: Update default test config with sane values for running tests

--- tests/BFF/Config/production.php
+++ tests/BFF/Config/production.php
@@ -1,8 +1,27 @@
 <?php
 
 return [
+    'env' => 'production',
     'nodeId' => 1,
     'paths' => [
         'export' => '/tmp'
-    ]
+    ],
+    'db' => [
+        'host' => '127.0.0.1',
+        'user' => 'root',
+        'pass' => 'pw',
+        'dbname' => 'bff-test',
+        'charset' => 'utf8',
+        'pdo_persistent' => false
+    ],
+    'memcache' => [
+        'host' => '127.0.0.1',
+        'port' => '11211',
+        'pool' => 'bff-test'
+    ],
+    'redis' => [
+        'host' => '127.0.0.1',
+        'port' => 6379,
+        'blockTimeout' => 1
+    ],
 ];
